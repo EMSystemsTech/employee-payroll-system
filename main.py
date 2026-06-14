6d48d7f662a3637b33c6b77f486dffa6bb0ed104
Ebony Morrow <ebonymorrow1986@gmail.com>
2026-06-13 23:58:10 -0500

    Add Payroll FastAPI routes
    Added Payroll API endpoints for home, GET all payroll records, GET payroll by payroll_id, and POST payroll processing using employee_id and hours_worked.

---
+++ main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from services.payroll_service import (load_payroll, process_payroll)
+
+app = FastAPI()
+
+class PayrollInput(BaseModel):
+    employee_id: int
+    hours_worked: float
+    
+@app.get("/")
+def home():
+    return {"message": "Payroll API Running"}
+
+@app.get("/payroll")
+def get_payroll():
+    payroll = load_payroll()
+
+    return{"count": len(payroll), "payroll": payroll}
+
+@app.get("/payroll/id/{payroll_id}")
+def get_payroll_by_id(payroll_id: int):
+    payroll = load_payroll()
+
+    for record in payroll:
+        if record["payroll_id"] == payroll_id:
+            return record
+
+    return {"message": "Payroll record not found"}
+
+@app.post("/payroll")
+def add_payroll(payroll_input: PayrollInput):
+    payroll_record = process_payroll(
+        payroll_input.employee_id,
+        payroll_input.hours_worked)
+
+    if payroll_record:
+        return {"message": "Payroll processed successfully", "payroll": payroll_record}
+
+    return {"message": "Employee not found"}
+
+
